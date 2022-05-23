754b07ddad1741a57937f6affdb6f4b718adf6fc
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-23 11:20:41 -0300

    exercise 1 answer

--- src/components/exercicio1/index.jsx
+++ src/components/exercicio1/index.jsx
@@ -1,5 +1,14 @@
+import './index.css'
+
 function Exercicio1() {
-	return <h1 className='title'>Exercicio1</h1>
+	return (
+		<>
+			<h1 className='title'>Exercicio1</h1>
+			<div className="exercise1__square">
+				<p className="exercise1__paragraph">Hello, World!</p>
+			</div>
+		</>
+	)
 }
 
 export default Exercicio1
