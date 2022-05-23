f3b5920f62e2399436923557a9a2c43fb9da5409
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-23 13:33:22 -0300

    exercise 4 answer

--- src/components/exercicio4/index.jsx
+++ src/components/exercicio4/index.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react'
+import './index.css'
+
 function Exercicio4() {
-	return <h1 className='title'>Exercicio4</h1>
+	const [count, setCount] = useState(0)
+
+	const incrementCount = () => {
+		setCount(prevState => prevState + 1)
+	}
+
+	return (
+		<>
+			<h1 className='title'>Exercicio4</h1>
+			<p className='exercise4__paragraph'>{`Button has been clicked: ${count} times`}</p>
+			<button className="exercise4__button" onClick={incrementCount}>
+        Click Me
+      </button>
+		</>
+	)
 }
 
 export default Exercicio4
