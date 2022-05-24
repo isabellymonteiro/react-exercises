cb2ab65696262a6e8a477abaec6b7b294e0becff
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-24 17:00:27 -0300

    exercise 9: handled errors and loading state

--- src/components/exercicio9/index.jsx
+++ src/components/exercicio9/index.jsx
@@ -4,13 +4,22 @@ import './index.css'
 import exercicios from '@/exercicios'
 
 function Exercicio9() {
-  const [userList, setUserList] = useState('')
+  const [userList, setUserList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const fetchUsers = () => {
-    fetch('https://random-data-api.com/api/users/random_user?size=10')
-    .then(response => {
-      return response.json()
-    }).then(data => {
+  const fetchUsers = async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://random-data-api.com/api/users/random_user?size=10')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+      
+      const data = await response.json()
+      
       setUserList(data.map(user => {
         return (
           <UserCard 
@@ -23,8 +32,11 @@ function Exercicio9() {
             email={user.email}
           />
         )
-      })
-    )})
+      }))
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -35,9 +47,15 @@ function Exercicio9() {
     <>
       <h1 className='title'>{exercicios[8]}</h1>
       <button className="fetch__button" onClick={fetchUsers}>Fetch Random</button>
-      <ul className='users'>
-        {userList}
-      </ul>
+      {isLoading && <p className='users--loading'>Loading...</p>}
+      {!isLoading && !error && (
+        <ul className='users'>
+          {userList}
+        </ul>
+      )}
+      {!isLoading && error && (
+        <p className='users--error'>{error}</p>
+      )}
     </>
   )
 }
